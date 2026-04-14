66774c90bb8c2fd6155639ed14629986fd01fab4
Max Dang Vu <mdan066@aucklanduni.ac.nz>
2026-04-14 17:52:55 +1200

    chore: removed redundant script.

--- projects/main.py
+++ projects/main.py
@@ -1,24 +1,59 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import pyvista as pv
+
 import sys
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print("0. Prelude")
+print("Hello World from Docker!")
+print(f"This is the {__file__} file.")
+print(f"Current Python version: {sys.version}.")
 
+print("========\n1. Testing Numpy...")
+print("Identity is:\n", np.eye(3))
+X = np.arange(0, 11, 0.1)
+Y = X**2 - 10*X + 21
+Z = np.zeros(shape=X.shape)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("========\n2. Testing Matplotlib...")
+root_idxs = np.where(Y == 0)[0]
+plt.plot(X, Y)
+plt.scatter(
+    X[root_idxs], Y[root_idxs], zorder=20,
+    marker='*', c='k', label='Roots'
+)
+plt.axhline(
+    y=0, linestyle="--",
+    color='red', label='$Y = 0$'
+)
 
-    print("Hello World from Docker!")
-    print("This is a test of the test.py file.")
-    print("The current version of Python is:", sys.version)
+plt.grid()
+plt.xlim([np.min(X), np.max(X)])
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.title(r'$Y = X^2 - 10X + 21$')
+plt.show()
+print("Matplotlib plotted.")
 
-    print("Identity is:\n", np.eye(3))
+print("========\n3. Testing PyVista...")
+mesh = pv.Sphere()
+plotter = pv.Plotter()
+plotter.add_mesh(mesh)
+plotter.add_axes()
+plotter.add_title("Sphere Mesh")
+plotter.show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plotter = pv.Plotter()
+plotter.add_points(
+    points=np.vstack((X, Y, Z)).T,
+    render_points_as_spheres=True,
+    color='green', label='Y = X^2 - 10X + 21'
+)
+plotter.add_axes()
+plotter.add_legend(bcolor=None)
+plotter.show()
+
+print("Pyvista plotted.")
